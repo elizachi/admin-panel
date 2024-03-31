ae3675d06d31a0fef0f194c5821b8bb57081aab5
elizachi <lizachig2003@gmail.com>
2024-03-31 22:52:25 +0300

    Added logout route

---
+++ app/src/Controller/LogoutController.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController extends AbstractController {
+    #[Route('/logout', name: 'logot', methods: ['POST'])]
+    public function login(): Response {
+        setCookie('login', '', time() - 3600, '/');
+        return new Response('User successfully logout');
+    }
+}
