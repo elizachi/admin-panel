d372ceb878c470773485feb7b5fe1ee936b16b2d
elizachi <lizachig2003@gmail.com>
2024-03-31 22:27:31 +0300

    Added twig files for main and login pages

--- app/src/Controller/LoginController.php
+++ app/src/Controller/LoginController.php
@@ -8,10 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginController extends AbstractController {
     #[Route('/login', name: 'login', methods: ['GET'])]
     public function login(): Response {
-        ob_start();
-        include $this->getParameter('kernel.project_dir') . '/templates/login.php';
-        $content = ob_get_clean();
-
-        return new Response($content);
+        return $this->render('login.html.twig');
     }
 }
--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -7,39 +7,33 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\UserService;
 
-class MainController extends AbstractController {
+class MainController extends AbstractController
+{
 
-    public function __construct(private readonly UserService $useUserService) {
-    }
-
-    private function showData(Request $request): Response {
-        if($request->cookies->has('login')) {
-            ob_start();
-            include $this->getParameter('kernel.project_dir') . '/templates/main/index.php';
-            $content = ob_get_clean();
-        } else {
-            ob_start();
-            include $this->getParameter('kernel.project_dir') . '/templates/main/unauthorized.php';
-            $content = ob_get_clean();
-        }
-        return new Response($content);
+    public function __construct(private readonly UserService $useUserService)
+    {
     }
 
     #[Route('/', name: 'load', methods: ['GET'])]
-    public function loadAction(Request $request): Response {
-
-        return $this->showData($request);
+    public function loadAction(Request $request): Response
+    {
+        if($request->cookies->has('login')) {
+            return $this->render('main/index.html.twig');
+        } else {
+            return $this->render('main/unauthorized.html.twig');
+        }
     }
 
     #[Route('/', name: 'buttonLog', methods: ['POST'])]
-    public function loginAction(Request $request): Response {
-
+    public function loginAction(Request $request): Response
+    {
         $user = $this->useUserService->findByLoginAndPassword($request);
 
         if ($user != null) {
             setCookie('login', $user->getLogin(), 0, '/');
+            return new Response('User successfully authorized', Response::HTTP_OK);
         }
 
-        return $this->showData($request);
+        return new Response('Please, ry again', Response::HTTP_BAD_REQUEST);
     }
 }
